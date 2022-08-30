38f7d4e427de49c40e2563ff409616d65487cf5d
phamquoctai <phamquoctai09082001@gmail.com>
2022-08-30 23:11:00 +0700

    directory structure

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Body from './Body/Body';
+import Footer from './Footer/Footer';
+import Navbar from './Navbar/Navbar';
+
+const HomeComponent = () => {
+  return (
+    <>
+      <Navbar/>
+      <Body/>
+      <Footer/>
+    </>
+  )
+}
+export default HomeComponent;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomeComponent from '../../components/Home';
+
+const Home = () => {
+  return (
+    <div>
+      <HomeComponent></HomeComponent>
+    </div>
+  )
+}
+
+export default Home
