0051cd2776a0177fdb06f4d4b62a99dd7231efdc
Tài Lê <letai97md@gmail.com>
2022-10-08 12:32:34 +0700

    AdminOverviewV1: handle function create and update product

--- src/components/admin/imageUpload/imageUpload.jsx
+++ src/components/admin/imageUpload/imageUpload.jsx
@@ -2,14 +2,15 @@ import React, { useState } from "react";
 import ImageUploading from "react-images-uploading";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import HighlightOffIcon from "@mui/icons-material/HighlightOff";
-export const ImageUpload = () => {
+export const ImageUpload = (prop) => {
   const [images, setImages] = useState([]);
   const maxNumber = 5;
-
+ 
   const onChange = (imageList, addUpdateIndex) => {
-    // data for submit
-    console.log(imageList, addUpdateIndex);
     setImages(imageList);
+    console.log(imageList);
+    prop.listImg(imageList)
+    
   };
   return (
     <div className="imageUpload">
@@ -35,25 +36,26 @@ export const ImageUpload = () => {
               className="drop__image"
               style={isDragging ? { color: "red" } : undefined}
               {...dragProps}
+              onClick={onImageUpload}
             >
               <CloudUploadIcon className="icon__upload" />
-              <p>DROP HERE</p>
-              <div className="listImg">
-                  {imageList.map((image, index) => (
-                    <div
-                      key={index}
-                      className="image-item mx-2 my-2 position-relative"
-                    >
-                      <img src={image["data_url"]} alt="" width="100" />
-                      <div className="image-item__btn-wrapper">
-                        <HighlightOffIcon
-                          onClick={() => onImageRemove(index)}
-                          className="btn-delete"
-                        />
-                      </div>
-                    </div>
-                  ))}
-              </div>
+              <p>Click or drop here</p>
+            </div>
+            <div className="listImg">
+              {images.map((image, index) => (
+                <div
+                  key={index}
+                  className="image-item mx-2 my-2 position-relative"
+                >
+                  <img src={image["data_url"]} alt="" width="100" />
+                  <div className="image-item__btn-wrapper">
+                    <HighlightOffIcon
+                      onClick={() => onImageRemove(index)}
+                      className="btn-delete"
+                    />
+                  </div>
+                </div>
+              ))}
             </div>
             <button onClick={onImageUpload} className="btn btn__upload">
               Upload image
--- src/components/admin/table/tableProduct.jsx
+++ src/components/admin/table/tableProduct.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -8,169 +8,49 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
-import {useNavigate} from "react-router-dom"
-export const TableProduct = () => {
-  const navigate = useNavigate()
-  const data = [
-    {
-      name: "áhháhd",
-      phone: "1231231",
-      address: "sadahsda",
-      province: "ádháudiha",
-      email: "ádhaoíhd",
-    },
-    {
-      name: "áhháhd",
-      phone: "1231231",
-      address: "sadahsda",
-      province: "ádháudiha",
-      email: "ádhaoíhd",
-    },
-    {
-      name: "áhháhd",
-      phone: "1231231",
-      address: "sadahsda",
-      province: "ádháudiha",
-      email: "ádhaoíhd",
-    },
-    {
-      name: "áhháhd",
-      phone: "1231231",
-      address: "sadahsda",
-      province: "ádháudiha",
-      email: "ádhaoíhd",
-    },
-    {
-      name: "áhháhd",
-      phone: "1231231",
-      address: "sadahsda",
-      province: "ádháudiha",
-      email: "ádhaoíhd",
-    },
-    {
-      name: "áhháhd",
-      phone: "1231231",
-      address: "sadahsda",
-      province: "ádháudiha",
-      email: "ádhaoíhd",
-    },
-    {
-      name: "áhháhd",
-      phone: "1231231",
-      address: "sadahsda",
-      province: "ádháudiha",
-      email: "ádhaoíhd",
-    },
-    {
-      name: "áhháhd",
-      phone: "1231231",
-      address: "sadahsda",
-      province: "ádháudiha",
-      email: "ádhaoíhd",
-    },
-    {
-      name: "áhháhd",
-      phone: "1231231",
-      address: "sadahsda",
-      province: "ádháudiha",
-      email: "ádhaoíhd",
-    },
-    {
-      name: "áhháhd",
-      phone: "1231231",
-      address: "sadahsda",
-      province: "ádháudiha",
-      email: "ádhaoíhd",
-    },
-    {
-      name: "áhháhd",
-      phone: "1231231",
-      address: "sadahsda",
-      province: "ádháudiha",
-      email: "ádhaoíhd",
-    },
-    {
-      name: "áhháhd",
-      phone: "1231231",
-      address: "sadahsda",
-      province: "ádháudiha",
-      email: "ádhaoíhd",
-    },
-    {
-      name: "áhháhd",
-      phone: "1231231",
-      address: "sadahsda",
-      province: "ádháudiha",
-      email: "ádhaoíhd",
-    },
-    {
-      name: "áhháhd",
-      phone: "1231231",
-      address: "sadahsda",
-      province: "ádháudiha",
-      email: "ádhaoíhd",
-    },
-    {
-      name: "áhháhd",
-      phone: "1231231",
-      address: "sadahsda",
-      province: "ádháudiha",
-      email: "ádhaoíhd",
-    },
-    {
-      name: "áhháhd",
-      phone: "1231231",
-      address: "sadahsda",
-      province: "ádháudiha",
-      email: "ádhaoíhd",
-    },
-    {
-      name: "áhháhd",
-      phone: "1231231",
-      address: "sadahsda",
-      province: "ádháudiha",
-      email: "ádhaoíhd",
-    },
-    {
-      name: "áhháhd",
-      phone: "1231231",
-      address: "sadahsda",
-      province: "ádháudiha",
-      email: "ádhaoíhd",
-    },
-    {
-      name: "áhháhd",
-      phone: "1231231",
-      address: "sadahsda",
-      province: "ádháudiha",
-      email: "ádhaoíhd",
-    },
-    {
-      name: "áhháhd",
-      phone: "1231231",
-      address: "sadahsda",
-      province: "ádháudiha",
-      email: "ádhaoíhd",
-    },
-    {
-      name: "áhháhd",
-      phone: "1231231",
-      address: "sadahsda",
-      province: "ádháudiha",
-      email: "ádhaoíhd",
-    },
-    {
-      name: "áhháhd",
-      phone: "1231231",
-      address: "sadahsda",
-      province: "ádháudiha",
-      email: "ádhaoíhd",
-    },
-  ];
+import { useNavigate } from "react-router-dom";
 
-  const moveToDetail = (abc) =>{
-    console.log("áđán");
-    navigate(`/product-management/${abc}`)
+import axios from "axios";
+
+export const TableProduct = () => {
+  const [data, setData] = useState([]);
+  const [checkDelete, setCheckDelete] = useState(false)
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    const fetchData = () => {
+      let url = `http://api-rental-carl.herokuapp.com/car`;
+      axios
+        .get(url)
+        .then(function (response) {
+          // handle success
+          setData(response.data);
+          console.log(response.data);
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+    };
+    fetchData();
+  }, [checkDelete]);
+
+  const moveToDetail = (_id) => {
+    navigate(`/product-management/${_id}`);
+  };
+
+  const deteleProduct =(e, id) =>{
+    e.stopPropagation();
+    axios
+    .delete(`http://api-rental-carl.herokuapp.com/car/${id}`)
+    .then(function (response) {
+      console.log(response);
+      setCheckDelete(!checkDelete);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+
   }
   return (
     <div className="tableProduct">
@@ -189,18 +69,20 @@ export const TableProduct = () => {
           </TableHead>
           <TableBody>
             {data?.map((item, index) => (
-              <TableRow key={index} className="productRow" onClick= {() => moveToDetail(index)}>
+              <TableRow
+                key={item._id}
+                className="productRow"
+                onClick={() => moveToDetail(item._id)}
+              >
                 <TableCell component="th" scope="row">
-                  {index}
+                  {item._id}
                 </TableCell>
-                <TableCell align="left">{item.name}</TableCell>
-                <TableCell align="left">{item.phone}</TableCell>
-                <TableCell align="left">
-                  {item.address + " " + item.province}
-                </TableCell>
-                <TableCell align="left">{item.email}</TableCell>
+                <TableCell align="left">{item?.name}</TableCell>
+                <TableCell align="left">{item?.genre?.label}</TableCell>
+                <TableCell align="left">{item?.seats}</TableCell>
+                <TableCell align="left">{item?.fuel}</TableCell>
                 <TableCell align="left" className="action">
-                  <div className="icon-action">
+                  <div className="icon-action" onClick={(e) => deteleProduct(e, item._id)}>
                     <HighlightOffIcon />
                   </div>
                   <div
