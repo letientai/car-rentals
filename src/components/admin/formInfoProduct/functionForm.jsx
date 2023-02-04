200921a0c86037d6a8b7c0d43fb511d208806c1d
PQTai <phamquoctai09082001@gmail.com>
2023-02-04 10:51:53 +0700

    Fix bug call api create authenInfo

--- src/components/admin/formInfoProduct/functionForm.jsx
+++ src/components/admin/formInfoProduct/functionForm.jsx
@@ -1,13 +1,14 @@
 const axios = require("axios").default;
 
-export const fetchGenre = () =>{
-    axios
-    .get(`http://api-rental-carl.herokuapp.com/genre`)
+const url = process.env.REACT_APP_URL_LOCAL;
+export const fetchGenre = () => {
+  axios
+    .get(`${url}genre`)
     .then(function (response) {
-      return response
+      return response;
     })
     .catch(function (error) {
       console.log("error", error);
-      return error
+      return error;
     });
-}
+};
